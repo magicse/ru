19ffc3c3cd3863cad6a42c58c44cd1d465406a06
magicse <magicse@users.noreply.github.com>
2017-06-11 01:21:47 +0300

    Create default.py
    правки. Возврат работоспособности

--- plugin.video.tushkan.net/default.py
+++ plugin.video.tushkan.net/default.py
@@ -138,6 +138,7 @@ def openitem(params):
 
     #ttt=re.compile('title="([^"]+)"\s*src="([^"]+)"><\/div>').findall(http)
     ttt=re.compile('title="([^"]+)"\s*src="([^"]+)">').findall(http)
+    ttt=re.compile('itemprop="([^"]+)"\s*src="([^"]+)">').findall(http)
     img="http://tushkan.club"+ttt[0][1]
     ttt2=ttt[0][0]
     hh= re.compile('http://.+film/.+.flv')
